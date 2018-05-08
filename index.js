665c1f2894e35ea9b7028bcaf1db169f14e887b0
Huande <38550894+Huande@users.noreply.github.com>
2018-05-08 06:45:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const TOKEN = '416125571:AAHzckK4qQwWU0c5u2AkUxS10ZsmRj7rkHk'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
+if message.text = 'salom'{
 bot.on('message', msg => {
 bot.sendMessage(msg.chat.id,'Husniddindan salom.')
-})
+}})
